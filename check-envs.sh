006f58f3095c0016c6ba4c2459442786b75a38d5
David Blane <david.blane@digicatapult.org.uk>
2024-03-12 11:40:18 +0000

    Source things correctly...

--- check-envs.sh
+++ check-envs.sh
@@ -13,6 +13,16 @@ if [ ! "${POT_MOUNT_BASE}" ] ; then
 	POT_MOUNT_BASE=/opt/pot/
 	echo POT_MOUNT_BASE not set, using ${POT_MOUNT_BASE}
 fi
+if [ -z "$GITHUB_TOKEN" ] || [ -z "$GITHUB_ORG" ]; then
+	echo "One or both environment variables are unset. Sourcing them from /root/.profile..."
+	. /root/.profile
+		# Re-check to confirm they are now set
+		if [ -z "$GITHUB_TOKEN" ] || [ -z "$GITHUB_ORG" ]; then
+			echo "Error: Environment variables could not be sourced from /root/.profile."
+		fi
+	else
+		echo "Both environment variables are already set."
+fi
 # Set the pot name to use underscores in place of dots (the one character pot
 # names are apparently not allowed).
 # FIXME: We shouldn't be allowing anything that isn't allowed in a path
