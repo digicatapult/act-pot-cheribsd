140065caa2a1840cd5d78a0ec41350b475a6bfbf
Elise Haldane <107867060+elisehaldane@users.noreply.github.com>
2024-03-14 18:26:00 +0000

    Move token automation to a new script

--- create-runner.sh
+++ create-runner.sh
@@ -1,24 +1,6 @@
 #!/bin/sh
 set -eo pipefail
 
-# Ensure GITHUB_PAT and GITHUB_ORG environment variables are set
-if [ -z "$GITHUB_PAT" ] || [ -z "$GITHUB_ORG" ]; then
-    echo "Error: GITHUB_PAT and GITHUB_ORG environment variables must be set."
-    exit 1
-fi
-
-# Generate a new GitHub Actions runner registration token for the organization
-TOKEN=$(curl -s -X POST -H "Authorization: token $GITHUB_PAT" \
-                  -H "Accept: application/vnd.github+json" \
-                  "https://api.github.com/orgs/$GITHUB_ORG/actions/runners/registration-token" | grep "token" | cut -d '"' -f 4)
-
-if [ "$TOKEN" = "null" ]; then
-    echo "Failed to generate token. Check if your GITHUB_PAT is correct and has the required permissions."
-    exit 1
-fi
-
-echo "Generated Token: $TOKEN"
-
 EXTRA_FLAVOURS=
 if [ "${RUNNER_FLAVOURS}" ] ; then
     for F in $RUNNER_FLAVOURS ; do
@@ -47,9 +29,6 @@ if [ ! -d $SIBLING_DIR ]; then
     pot stop -p sibling
     pot snap -p sibling
 fi
-
-# Save the token in a place where the github-act-configure.sh script can access it
-echo $TOKEN > token
 
 pot clone -p ${POTNAME} -P sibling -f github-act ${EXTRA_FLAVOURS} \
     -f github-act-configured
--- flavours/github-act-configure.sh
+++ flavours/github-act-configure.sh
@@ -8,20 +8,13 @@ if [ -f "$CONFIG_FILE" ]; then
     cat "$CONFIG_FILE"
 fi
 
-# Load the token from the file saved by create-runner.sh
-if [ ! -f "/root/token" ]; then
-    echo "Runner token file not found."
-    exit 1
-fi
-GITHUB_TOKEN=$(cat /root/token)
-
 ARCH=$(ls /usr/local/share/freebsd/MANIFESTS | \
     grep -Eo "\w{1,}\.\w{1,}" | sort -u)
 CHERIBSD_BUILD_ID=$(echo ${ARCH} | awk -F " " '{print $NF}')
 # Configure the runner
 cd /root/runner
 GODEBUG="asyncpreemptoff=1" /usr/local64/bin/github-act-runner configure \
-    --url "https://github.com/${GITHUB_ORG}" \
+    --url "${GITHUB_URL}" \
     --token "${GITHUB_TOKEN}" \
     --name "${POTNAME}" \
     --labels cheribsd,"cheribsd-${CHERIBSD_BUILD_ID}" \
