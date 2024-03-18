5a3dc9de8de99cdddf10ddd7aeb3b23bc092598b
David Blane <david.blane@digicatapult.org.uk>
2024-03-18 11:13:02 +0000

    Removing trailing slash on envar as we end up double-slashing in a few places.

--- check-envs.sh
+++ check-envs.sh
@@ -10,7 +10,7 @@ if [ ! "${RUNNER_NAME}" ] ; then
 	echo RUNNER_NAME not set, using ${RUNNER_NAME}
 fi
 if [ ! "${POT_MOUNT_BASE}" ] ; then
-	POT_MOUNT_BASE=/opt/pot/
+	POT_MOUNT_BASE=/opt/pot
 	echo POT_MOUNT_BASE not set, using ${POT_MOUNT_BASE}
 fi
 # Set the pot name to use underscores in place of dots (the one character pot
